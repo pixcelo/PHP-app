4b1d1503820e8c0c8f33808b5530fc852fb3e24e
pixcelo <bodomu@gmail.com>
2024-04-27 05:12:58 +0900

    update: storeメソッド

--- docker-laravel-handson/src/app/Http/Controllers/ActionController.php
+++ docker-laravel-handson/src/app/Http/Controllers/ActionController.php
@@ -6,5 +6,15 @@ use Illuminate\Http\Request;
 
 class ActionController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        $validateData = $request->validate([
+            'name' => 'required',
+            'goal_id' => 'required|exists:goals,id',
+        ]);
+
+        $action = Action::create($validateData);
+
+        return redirect()->route('actions.show', $action)->with('success', 'タスクが作成されました。');
+    }
 }
--- docker-laravel-handson/src/app/Http/Controllers/GoalController.php
+++ docker-laravel-handson/src/app/Http/Controllers/GoalController.php
@@ -6,5 +6,15 @@ use Illuminate\Http\Request;
 
 class GoalController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        $validationData = $request->validate([
+            'name' => 'required',
+            'category' => 'required',
+        ]);
+    }
+
+    $goal = Goal::crate($validateData);
+
+    return redirect()->route('goals.show', $goal)->with('success', '目標が作成されました。');
 }
