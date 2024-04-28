6bd5d7575021e26f0d53f0558f5606d4756ba11e
pixcelo <bodomu@gmail.com>
2024-04-28 14:22:47 +0900

    fix: シンタックスエラー修正

--- docker-laravel-handson/src/app/Http/Controllers/GoalController.php
+++ docker-laravel-handson/src/app/Http/Controllers/GoalController.php
@@ -6,15 +6,24 @@ use Illuminate\Http\Request;
 
 class GoalController extends Controller
 {
+    public function index()
+    {
+        $goals = Goal::orderBy('id', 'asc')->get();
+        
+        return view('goal_list', [
+            'goals' => $goals
+        ]);
+    }
+
     public function store(Request $request)
     {
         $validationData = $request->validate([
             'name' => 'required',
             'category' => 'required',
         ]);
-    }
 
-    $goal = Goal::crate($validateData);
+        $goal = Goal::crate($validateData);
 
-    return redirect()->route('goals.show', $goal)->with('success', '目標が作成されました。');
+        return redirect()->route('goals.show', $goal)->with('success', '目標が作成されました。');
+    }    
 }
